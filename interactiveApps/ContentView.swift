e4049ba57c88212ab5140f9ef70fabb344e2a463
Eason Ong <eong@lcs.on.ca>
2024-01-15 09:29:46 -0500

    Initial Commit

---
+++ interactiveApps/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  interactiveApps
+//
+//  Created by Ong Eason on 15/1/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ interactiveApps/interactiveAppsApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactiveAppsApp.swift
+//  interactiveApps
+//
+//  Created by Ong Eason on 15/1/2024.
+//
+
+import SwiftUI
+
+@main
+struct interactiveAppsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
